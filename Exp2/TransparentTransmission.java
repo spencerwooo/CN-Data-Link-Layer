3f7d14bb6b124a64fcd483ac12cb15927a6953e7
spencerwooo <spencerwoo98@gmail.com>
2019-04-21 17:17:55 +0800

    Add byte stuffing

--- Exp2/TransparentTransmission.java
+++ Exp2/TransparentTransmission.java
@@ -78,35 +78,70 @@ public class TransparentTransmission {
   /*
    * 字节填充
    */
-  public static String byteStuffing(String infoString) {
+  public static String byteStuffingEncode(String infoString) {
     System.out.println("[INFO] Byte Stuffing Starting...");
-    String flagString = "7E";
-    String[] subStrings = infoString.split(flagString);
+    System.out.println("Source data: " + infoString);
 
-    try {
-      if (subStrings.length >= 3) {
-        System.out.println(subStrings[0]);
-        System.out.println(subStrings[1]);
-        System.out.println(subStrings[2]);
+    String result = new String();
+
+    for (int i = 0; i < infoString.length(); i++) {
+      if (infoString.charAt(i) == '7') {
+        if (infoString.charAt(i + 1) == 'E') {
+          result += '/';
+          result += infoString.charAt(i);
+        } else {
+          result += infoString.charAt(i);
+        }
+      } else if (infoString.charAt(i) == '/') {
+        result += '/';
+        result += infoString.charAt(i);
       } else {
-        throw new Exception("Illegal string.");
+        result += infoString.charAt(i);
       }
-    } catch (Exception e) {
-      System.out.println(e);
-      System.exit(0);
     }
 
-    return infoString;
+    System.out.println("Sender string: " + byteFlagString + " | " + result + " | " + byteFlagString);
+    result = byteFlagString + result + byteFlagString;
+    return result;
+  }
+
+  public static String byteStuffingDecode(String encodedString) {
+    System.out.println("[INFO] Received! Decoding...");
+
+    String result = new String();
+
+    for (int i = 0; i < encodedString.length(); i++) {
+      if (encodedString.charAt(i) == '/') {
+        if (encodedString.charAt(i + 1) == '/') {
+          result += encodedString.charAt(i);
+          i++;
+        } else if (encodedString.charAt(i + 1) == '7') {
+          if (encodedString.charAt(i + 2) == 'E') {
+            result += encodedString.charAt(i + 1);
+            i++;
+          } else {
+            result += encodedString.charAt(i);
+          }
+        } else {
+          result += encodedString.charAt(i);
+        }
+      } else {
+        result += encodedString.charAt(i);
+      }
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
-    // String byteStuffString = "347D7E807E40AA7D";
-    // String byteSenderString = byteStuffing(byteStuffString);
-    // System.out.println(byteSenderString);
-
     String zeroBitStuffString = "0110011111101111111111101111110110";
     String zeroBitSenderString = zeroBitStuffingEncode(zeroBitStuffString);
     String decodeZeroBitSender = zeroBitStuffingDecode(zeroBitSenderString);
     System.out.println("Receiver string: " + decodeZeroBitSender);
+    
+    String byteStuffString = "347D7E807E40AA7D";
+    String byteSenderString = byteStuffingEncode(byteStuffString);
+    String decodeByteString = byteStuffingDecode(byteSenderString);
+    System.out.println("Receiver string: " + decodeByteString);
   }
 }
