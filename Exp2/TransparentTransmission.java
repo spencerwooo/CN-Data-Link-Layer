d90f456b3b47ae39c8f18bb5d49d9848bf413073
spencerwooo <spencerwoo98@gmail.com>
2019-04-21 16:47:54 +0800

    Add bit stuffing

--- Exp2/TransparentTransmission.java
+++ Exp2/TransparentTransmission.java
@@ -2,28 +2,77 @@
  * TransparentTransmission
  */
 public class TransparentTransmission {
+  public static final String zeroBitFlagString = "01111110";
+  public static final String byteFlagString = "7E";
+
   /*
    * 零比特填充
    */
-  public static String zeroBitStuffing(String infoString) {
-    System.out.println("[INFO] Zeo Bit Stuffing Starting...");
-    String flagString = "01111110";
-    String[] subStrings = infoString.split(flagString);
+  public static String zeroBitStuffingEncode(String infoString) {
+    System.out.println("[INFO] Zero Bit Stuffing Starting...");
+    System.out.println("Source data: " + infoString);
 
+    String result = new String();
+
+    int counter = 0;
     try {
-      if (subStrings.length >= 3) {
-        System.out.println(subStrings[0]);
-        System.out.println(subStrings[1]);
-        System.out.println(subStrings[2]);
-      } else {
-        throw new Exception("Illegal string.");
+      for (int i = 0; i < infoString.length(); i++) {
+        if (infoString.charAt(i) != '1' && infoString.charAt(i) != '0') {
+          throw new Exception("Illegal string, only binary values allowed.");
+        }
+
+        if (infoString.charAt(i) == '1') {
+          counter++;
+          result += infoString.charAt(i);
+        } else {
+          result += infoString.charAt(i);
+          counter = 0;
+        }
+
+        if (counter == 5) {
+          result += '0';
+          counter = 0;
+        }
       }
     } catch (Exception e) {
       System.out.println(e);
       System.exit(0);
     }
 
-    return infoString;
+    System.out.println("Sending string: " + zeroBitFlagString + " | " + result + " | " + zeroBitFlagString);
+
+    result = zeroBitFlagString + result + zeroBitFlagString;
+    return result;
+  }
+
+  public static String zeroBitStuffingDecode(String encodedString) {
+    System.out.println("Received! Decoding...");
+    String result = new String();
+    int counter = 0;
+
+    encodedString = encodedString.split(zeroBitFlagString)[1];
+
+    for (int i = 0; i < encodedString.length(); i++) {
+      if (encodedString.charAt(i) == '1') {
+        counter++;
+        result += encodedString.charAt(i);
+      } else {
+        result += encodedString.charAt(i);
+        counter = 0;
+      }
+
+      if (counter == 5) {
+        if ((i + 2) != encodedString.length()) {
+          result += encodedString.charAt(i + 2);
+        } else {
+          result += '1';
+        }
+        i += 2;
+        counter = 1;
+      }
+    }
+
+    return result;
   }
 
   /*
@@ -51,12 +100,13 @@ public class TransparentTransmission {
   }
 
   public static void main(String[] args) {
-    String byteStuffString = "347D7E807E40AA7D";
-    String byteSenderString = byteStuffing(byteStuffString);
-    System.out.println(byteSenderString);
+    // String byteStuffString = "347D7E807E40AA7D";
+    // String byteSenderString = byteStuffing(byteStuffString);
+    // System.out.println(byteSenderString);
 
     String zeroBitStuffString = "0110011111101111111111101111110110";
-    String zeroBitSenderString = zeroBitStuffing(zeroBitStuffString);
-    System.out.println(zeroBitSenderString);
+    String zeroBitSenderString = zeroBitStuffingEncode(zeroBitStuffString);
+    String decodeZeroBitSender = zeroBitStuffingDecode(zeroBitSenderString);
+    System.out.println("Receiver string: " + decodeZeroBitSender);
   }
 }
