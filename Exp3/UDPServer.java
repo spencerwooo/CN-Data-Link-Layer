261fc74a49dd15747b4cf9bf639a0e1570ec5b1e
spencerwooo <spencerwoo98@gmail.com>
2019-04-24 12:08:17 +0800

    Finalize server comments

--- Exp3/UDPServer.java
+++ Exp3/UDPServer.java
@@ -7,31 +7,37 @@ import java.util.Arrays;
  * UDPServer
  */
 public class UDPServer {
-
+  // Server and client listens on different ports
   private static final int UDPServerPort = 8888;
   private static final int UDPClientPort = 9999;
 
+  // Add trailing zero (For DataHandler to print out byte array contents.)
   private static final byte[] zeroByte = { (byte) 0 };
 
   public static void main(String[] args) throws Exception {
     DatagramSocket socket = new DatagramSocket(UDPServerPort);
+    InetAddress ip = InetAddress.getByName("localhost");
+    DataHandler dataHandler = new DataHandler();
+
     byte[] senderBuffer = new byte[65536];
     byte[] receiverBuffer = new byte[65536];
     byte[] expectedChecksumRaw = new byte[65536];
 
-    InetAddress ip = InetAddress.getByName("localhost");
+    int index = 0;
+    int invalid = -1;
 
     System.out.println("[INFO] Server listening on port: " + UDPServerPort);
 
-    DataHandler dataHandler = new DataHandler();
-
-    int index = 0;
-    int invalid = -1;
+    // Receive a total of 24 frames
     while (index < 24) {
+      // Receive packets
       DatagramPacket receiverDataPacket = new DatagramPacket(receiverBuffer, receiverBuffer.length);
       socket.receive(receiverDataPacket);
+
       // Trim trailing zeros from received packet
       receiverBuffer = dataHandler.trimByteArray(receiverBuffer);
+
+      // Frame received should be like: [Frame] + [0] + [Checksum]
       System.out.println(Arrays.toString(receiverBuffer));
 
       // Find inserted zero in byte stream
@@ -43,8 +49,6 @@ public class UDPServer {
         pointer++;
       }
 
-      System.out.println(pointer);
-
       // Get received buffer and expected checksum
       expectedChecksumRaw = Arrays.copyOfRange(receiverBuffer, pointer + 1, receiverBuffer.length);
       expectedChecksumRaw = dataHandler.appendByteArray(expectedChecksumRaw, zeroByte);
@@ -54,26 +58,30 @@ public class UDPServer {
       String expectedChecksum = dataHandler.byteStreamToString(expectedChecksumRaw);
       System.out.println("Actual: " + actualChecksum + " | Expected: " + expectedChecksum);
 
+      // Check if checksum is expected
       if (actualChecksum.equals(expectedChecksum)) {
+        // Packet is valid, return packet index to client.
         System.out.println(
             "Client sent packet " + index + ": " + dataHandler.byteStreamToString(receiverBuffer) + " is as expected.");
 
         senderBuffer = String.valueOf(index).getBytes();
-
         DatagramPacket senderDataPacket = new DatagramPacket(senderBuffer, senderBuffer.length, ip, UDPClientPort);
         socket.send(senderDataPacket);
 
+        // Packet index self-increase
         index++;
       } else {
+        // Packet is invalid, demand a resend.
         System.out.println(
             "Client sent packet " + index + ": " + dataHandler.byteStreamToString(receiverBuffer) + " is invalid.");
 
+        // Send back invalid value to client (invalid = -1)
         senderBuffer = String.valueOf(invalid).getBytes();
-
         DatagramPacket senderDataPacket = new DatagramPacket(senderBuffer, senderBuffer.length, ip, UDPClientPort);
         socket.send(senderDataPacket);
       }
 
+      // Re-init byte buffers
       receiverBuffer = new byte[65536];
       expectedChecksumRaw = new byte[65536];
     }
